949b59789d3be58319908eb1be05ea4a4bf364a9
hehoku <hehoku@outlook.com>
2023-11-19 22:47:59 +0800

    Refactor p5 sketch in App component and add basic sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import * as p5 from 'p5'
-import { ReactP5Wrapper, Sketch } from '@p5-wrapper/react'
+import { ReactP5Wrapper } from '@p5-wrapper/react'
+import Basic from './libs/basic'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ;(window as any).p5 = p5
-
-const sketch: Sketch = (p5) => {
-  p5.setup = () => p5.createCanvas(600, 500, p5.WEBGL)
-
-  p5.draw = () => {
-    p5.background(252, 247, 218)
-    p5.normalMaterial()
-  }
-}
 
 export default function App() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="rounded-md border-2 border-gray-200">
-        <ReactP5Wrapper sketch={sketch} />
+        <ReactP5Wrapper sketch={Basic} />
       </div>
     </div>
   )
---
+++ src/libs/basic.ts
@@ -0,0 +1,36 @@
+import * as p5 from 'p5'
+import { Sketch } from '@p5-wrapper/react'
+
+const Basic: Sketch = (p5) => {
+  const w = 600
+  const h = 500
+  p5.setup = () => p5.createCanvas(w, h, p5.WEBGL)
+
+  p5.draw = () => {
+    p5.background(0, 0, 0)
+    p5.fill(255, 255, 255)
+
+    drawCircle(p5, 0, 0, 100, 100)
+  }
+}
+
+function drawCircle(
+  p5: p5,
+  centerX: number,
+  centerY: number,
+  radius: number,
+  numPoints: number,
+) {
+  const angleStep = p5.TWO_PI / numPoints
+
+  p5.fill(255, 255, 255)
+  p5.beginShape()
+  for (let i = 0; i < numPoints; i++) {
+    const x = centerX + radius * p5.cos(i * angleStep)
+    const y = centerY + radius * p5.sin(i * angleStep)
+    p5.curveVertex(x, y)
+  }
+  p5.endShape(p5.CLOSE)
+}
+
+export default Basic

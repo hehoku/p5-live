ce52eebd652307c84a34cc4528d0932d76428015
hehoku <hehoku@outlook.com>
2023-11-21 23:59:51 +0800

    chore: dev auto refresh canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { ReactP5Wrapper, Sketch } from '@p5-wrapper/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Basic from './libs/basic'
 import Shape from './libs/shape'
+import Blend from './libs/blend'
 
 interface FileMap {
   [key: string]: Sketch
@@ -11,10 +12,12 @@ interface FileMap {
 const fileMap: FileMap = {
   Basic: Basic,
   Shape: Shape,
+  Blend: Blend,
 }
 
 export default function App() {
-  const [demo, setDemo] = useState(() => Shape)
+  const defaultDemo = Object.values(fileMap)[Object.values(fileMap).length - 1]
+  const [demo, setDemo] = useState(() => defaultDemo)
 
   const handleClick = (file: string) => {
     const selectedFile = fileMap[file]
@@ -22,6 +25,11 @@ export default function App() {
       setDemo(() => selectedFile)
     }
   }
+
+  // for dev auto refresh canvas
+  useEffect(() => {
+    setDemo(() => defaultDemo)
+  }, [defaultDemo])
 
   return (
     <div className="mx-auto flex min-h-screen w-2/3 flex-row items-center justify-center gap-x-10 px-10">
@@ -31,7 +39,7 @@ export default function App() {
             key={fileName}
             href={`#${fileName}`}
             onClick={() => handleClick(fileName)}
-            className='font-bold hover:underline active:text-blue-500'
+            className="font-bold hover:underline active:text-blue-500"
           >
             {fileName}
           </a>

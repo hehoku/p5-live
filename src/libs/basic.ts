4f5a6070e0623d54022d2be8ada080ce38a241c8
hehoku <hehoku@outlook.com>
2023-11-23 00:16:10 +0800

    use 2D mode

--- src/libs/basic.ts
+++ src/libs/basic.ts
@@ -1,35 +1,16 @@
-import { P5CanvasInstance, Sketch, SketchProps } from '@p5-wrapper/react'
+import { Sketch } from '@p5-wrapper/react'
 
 const Basic: Sketch = (p5) => {
   const w = 600
   const h = 500
-  p5.setup = () => p5.createCanvas(w, h, p5.WEBGL)
+  p5.setup = () => p5.createCanvas(w, h)
 
   p5.draw = () => {
     p5.background(0, 0, 0)
     p5.fill(255, 255, 255)
-
-    drawCircle(p5, 0, 0, 100, 100)
+    p5.translate(w / 2, h / 2)
+    p5.circle(0, 0, 250)
   }
-}
-
-function drawCircle(
-  p5: P5CanvasInstance<SketchProps>,
-  centerX: number,
-  centerY: number,
-  radius: number,
-  numPoints: number,
-) {
-  const angleStep = p5.TWO_PI / numPoints
-
-  p5.fill(255, 255, 255)
-  p5.beginShape()
-  for (let i = 0; i < numPoints; i++) {
-    const x = centerX + radius * p5.cos(i * angleStep)
-    const y = centerY + radius * p5.sin(i * angleStep)
-    p5.curveVertex(x, y)
-  }
-  p5.endShape(p5.CLOSE)
 }
 
 export default Basic
